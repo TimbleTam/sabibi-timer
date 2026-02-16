c4b5c751cbe24924cadaa6ef09d76a34f7f6f724
Liam Fortune <39064985+TimbleTam@users.noreply.github.com>
2026-02-16 11:48:05 +0000

    fixed routing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,21 @@
 <script setup lang="ts">
 function launchTimer() {
-  const width = 430
-  const height = 430
-  const left = (screen.width - width) / 2
-  const top = (screen.height - height) / 2
+  const width = 430;
+  const height = 430;
+  const left = (screen.width - width) / 2;
+  const top = (screen.height - height) / 2;
 
   window.open(
-    '/#/timer',
-    'sabibi-timer',
+    "/sabibi-timer/#/timer",
+    "sabibi-timer",
     `width=${width},height=${height},left=${left},top=${top},resizable=no,menubar=no,toolbar=no,location=no,status=no`,
-  )
+  );
 }
 </script>
 
 <template>
   <div class="home">
-    <button class="launch-btn" @click="launchTimer">
-      Start Timer
-    </button>
+    <button class="launch-btn" @click="launchTimer">Start Timer</button>
   </div>
 </template>
 
